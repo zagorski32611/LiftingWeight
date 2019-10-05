e44f64502b0315b6eb0891f4dee1b41fa21b97bf
Joe <j.j.zagorski@gmail.com>
2019-10-05 07:46:56 -0400

    added model display names Exercises

--- Models/Exercises.cs
+++ Models/Exercises.cs
@@ -12,10 +12,20 @@ namespace LiftingWeight.Models
             LiftingProgress = new HashSet<LiftingProgress>();
         }
         public int ExerciseId { get; set; }
+        
+        [Display(Name = "Exercise Name")]
         public string ExerciseName { get; set; }
+        
+        [Display(Name = "Machine or Free?")]
         public bool? MachineOrFree { get; set; }
+
+        [Display(Name = "Targeted Muscle")]
         public string TargetedMuscle { get; set; }
+
+        [Display(Name = "Gym or Home?")]
         public bool? GymOrHome { get; set; }
+
+        [Display(Name = "Current?")]
         public bool? Current { get; set; }
 
         public virtual ICollection<LiftingProgress> LiftingProgress { get; set; }
